5e82e728ae6a90436dfe1707236326b4ca933681
jbarribal <barribal.joshuageorge@ue.edu.ph>
2022-11-02 09:49:51 +0800

    change api

--- components/lib/helper.js
+++ components/lib/helper.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://employee-management-blush-xi.vercel.app"
 
 export const getUsers = async () => {
     const response = await fetch(`${BASE_URL}/api/users/`)
